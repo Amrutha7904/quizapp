14492f5364d32f5c26eacda0b6053419647ac60d
Amrutha O S <90167896+Amrutha7904@users.noreply.github.com>
2022-01-23 18:01:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -30,12 +30,14 @@ const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = docment.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+
 // functions
 let currentQuiz = 0;
  loadQuiz();
 
  function loadQuiz(){
-     const currentQuizData = quizData[currentQuiz];
+    const currentQuizData = quizData[currentQuiz];
+    
     questionE1.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
